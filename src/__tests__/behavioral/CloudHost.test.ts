50554ace247cd739d7813a102edb1fe0656ae8b3
eric-yates <eric@lumenalabs.com>
2024-07-21 14:40:05 -0600

    patch: create test file for CloudHost

---
+++ src/__tests__/behavioral/CloudHost.test.ts
@@ -0,0 +1,21 @@
+import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
+import CloudHostImpl, { CloudHost } from '../../CloudHost'
+
+export default class CloudHostTest extends AbstractSpruceTest {
+    private static host: CloudHost
+
+    protected static async beforeEach() {
+        await super.beforeEach()
+
+        this.host = this.CloudHost()
+    }
+
+    @test()
+    protected static async canCreateInstantLlm() {
+        assert.isTruthy(this.host)
+    }
+
+    private static CloudHost() {
+        return CloudHostImpl.Create()
+    }
+}
