ea33dec053a4d422ed796b6dd7540f09113ca8bb
eric-yates <hello@ericthecurious.com>
2024-07-27 17:46:56 -0600

    patch: refactor test

--- src/__tests__/behavioral/AzureHost.test.ts
+++ src/__tests__/behavioral/AzureHost.test.ts
@@ -2,14 +2,16 @@ import AbstractSpruceTest, { test, assert } from '@sprucelabs/test-utils'
 import AzureHost from '../../hosts/azure/AzureHost'
 
 export default class AzureHostTest extends AbstractSpruceTest {
+    private static host: AzureHost
+
     protected static async beforeEach() {
         await super.beforeEach()
+        this.host = this.AzureHost()
     }
 
     @test()
     protected static async canCreateAzureHost() {
-        const azureHost = this.AzureHost()
-        assert.isTruthy(azureHost)
+        assert.isTruthy(this.host)
     }
 
     private static AzureHost() {
